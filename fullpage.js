6e0ce4654cc1f6ccdc2364dc3498945bef13c52d
josslynmao <mao.huifeng@hotmail.com>
2015-12-12 20:13:25 +0800

    xxx

--- fullpage.js
+++ fullpage.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  $("#fullpage").fullpage();
+  $("#fullpage").fullpage({
+    sectionsColor: ['#59DFCE','#F7559F','#59DFCE','#F7559F'],
+    //controlArrows: false,
+    //verticalCentered: false,
+    scrollSpeed: 1000,
+    anchors: ['page1','page2','page3','page4'],
+    fixedElements: '#header'
+  });
 });
