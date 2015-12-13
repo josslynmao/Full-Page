01b60eb2f1445f23f1543aeeb3fdb62823048e35
josslynmao <mao.huifeng@hotmail.com>
2015-12-13 18:31:34 +0800

    徒手模仿一个苹果广告页

--- fullpage.js
+++ fullpage.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
   $("#fullpage").fullpage({
-    sectionsColor: ['#82E6D5','#F7559F','#82E6D5','#F7559F','#82E6D5'],
-    //controlArrows: false,
-    //verticalCentered: false,
+    verticalCentered: false,
     scrollSpeed: 700,
-    anchors: ['page1','page2','page3','page4'],
-    fixedElements: '#header',
+    anchors: ['page1','page2','page3','page4','page5'],
     navigation: true,
     navigationColor: 'white',
-    navigationTooltips: ['page1','page2','page3','page4'],
-    scrollOverflow: true,
-    onLeave: function(index,nextIndex,direction){
-      console.log("index:"+index+",nextIndex:"+nextIndex+",direction:"+direction);
-    },
-    afterRender: function(){
-      console.log("afterRender");
-    }
+    navigationTooltips: ['page1','page2','page3','page4','page5'],
+
   });
 });
