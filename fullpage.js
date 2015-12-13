a373eacab11fb375a38b0bce223408646e730a85
josslynmao <mao.huifeng@hotmail.com>
2015-12-13 16:47:33 +0800

    animation

--- fullpage.js
+++ fullpage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#fullpage").fullpage({
-    sectionsColor: ['#82E6D5','#F7559F','#82E6D5','#F7559F'],
+    sectionsColor: ['#82E6D5','#F7559F','#82E6D5','#F7559F','#82E6D5'],
     //controlArrows: false,
     //verticalCentered: false,
     scrollSpeed: 700,
@@ -9,6 +9,12 @@ $(document).ready(function(){
     navigation: true,
     navigationColor: 'white',
     navigationTooltips: ['page1','page2','page3','page4'],
-    scrollOverflow: true
+    scrollOverflow: true,
+    onLeave: function(index,nextIndex,direction){
+      console.log("index:"+index+",nextIndex:"+nextIndex+",direction:"+direction);
+    },
+    afterRender: function(){
+      console.log("afterRender");
+    }
   });
 });
