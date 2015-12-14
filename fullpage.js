044dbadbb517610bb2049833b28d134fc0e70325
josslynmao <mao.huifeng@hotmail.com>
2015-12-14 13:27:23 +0800

    page1

--- fullpage.js
+++ fullpage.js
@@ -6,5 +6,45 @@ $(document).ready(function(){
     navigation: true,
     navigationColor: 'white',
     navigationTooltips: ['page1','page2','page3','page4','page5'],
+
+    afterRender: function(){
+        move('.section1 h1').scale(1.5).duration('3s').end();
+        move('.section1 p').set('margin-top','5%').duration('3s').end();
+      },
+
+    afterLoad: function(anchorLink,index){
+        switch (index) {
+          case 1:
+            break;
+          case 2:
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+          case 5:
+          break;
+          default:
+          break;
+        }
+      },
+
+    onLeave: function(index,nextIndex,direction){
+        switch (index) {
+          case 1:
+            break;
+          case 2:
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+          case 5:
+          break;
+          default:
+          break;
+        }
+      }
+
   });
 });
