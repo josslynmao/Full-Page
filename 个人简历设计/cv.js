c2cfc2206cd86fffd3773b7cd1ae9863563a8831
josslynmao <mao.huifeng@hotmail.com>
2015-12-14 20:48:24 +0800

    xxxxxxxxx

---
+++ 个人简历设计/cv.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#fullPage").fullpage({
+    anchors: ['page1','page2','page3','page4'],
+    navigation: true,
+    verticalCentered: false,
+    fixedElements: '#header',
+    afterRender: function(){
+      $('.section1 img').fadeIn(2000);
+    },
+  });
+});
